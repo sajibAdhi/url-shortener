b3c58841a3060828ee9925ca959e62e71147e548
Sajib Adhikary <tosajibadhi@gmail.com>
2023-09-28 19:15:20 +0600

    cmd: php artisan make:livewire url-shortener
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

---
+++ app/Livewire/UrlShortener.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class UrlShortener extends Component
+{
+    public function render()
+    {
+        return view('livewire.url-shortener');
+    }
+}
